b45185e60049e3b9fb264c67edd90b2ea46a76f8
prasetyo507 <prasetyo507@gmail.com>
2019-04-25 16:55:29 +0700

    Update home.php

--- application/modules/home/controllers/home.php
+++ application/modules/home/controllers/home.php
@@ -6,7 +6,7 @@ class Home extends MX_Controller {
 	public function index()
 	{
 		$data['about'] = $this->model_crud->getDetail('tbl_about','id_about',1)->row();
-		$data['page'] = 'home/home/v_home';
+		$data['page'] = 'home/about/v_about';
 		$this->load->view('template/template',$data);
 	}
 }

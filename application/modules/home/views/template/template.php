e7532025fff220dad4dac93ef2d58c968a805c44
prasetyo507 <prasetyo507@gmail.com>
2019-05-02 14:27:12 +0700

    Update template.php

--- application/modules/home/views/template/template.php
+++ application/modules/home/views/template/template.php
@@ -88,9 +88,6 @@
 											<a class="page-scroll" href="#klien">Klien</a>
 										</li>
 										<li>
-											<a class="page-scroll" href="#faq">FAQ</a>
-										</li>
-										<li>
 											<a class="page-scroll" href="#portfolio">Portfolio</a>
 										</li>
 										<li>
@@ -98,12 +95,6 @@
 										</li>
 										<li>
 											<a class="page-scroll" href="#artikel">Artikel</a>
-										</li>
-										<li>
-											<a class="page-scroll" href="#testimonial">Testimonial</a>
-										</li>
-										<li>
-											<a class="page-scroll" href="#kontak">Kontak</a>
 										</li>
 									</ul>
 								</div>
@@ -450,11 +441,7 @@
 			<div class="suscribe-area">
 				<div class="container">
 					<div class="row">
-						<div class="col-lg-12 col-md-12 col-sm-12 col-xs=12">
-							<div class="suscribe-text text-center">
-								<?php echo $this->load->view($testimonial) ?>
-							</div>
-						</div>
+						<?php echo $this->load->view($testimonial) ?>
 					</div>
 				</div>
 			</div>
@@ -573,26 +560,26 @@
 				</div>
 			</div>
 			<div class="footer-area-bottom">
-			  <div class="container">
-				<div class="row">
-				  <div class="col-md-12 col-sm-12 col-xs-12">
-					<div class="copyright text-center">
-					  <p>
-						&copy; Copyright <strong>eBusiness</strong>. All Rights Reserved
-					  </p>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12 col-sm-12 col-xs-12">
+							<div class="copyright text-center">
+								<p>
+									&copy; Copyright <strong>eBusiness</strong>. All Rights Reserved
+								</p>
+							</div>
+							<div class="credits">
+								  <!--
+									All the links in the footer should remain intact.
+									You can delete the links only if you purchased the pro version.
+									Licensing information: https://bootstrapmade.com/license/
+									Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=eBusiness
+								  -->
+								Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+							</div>
+						</div>
 					</div>
-					<div class="credits">
-					  <!--
-						All the links in the footer should remain intact.
-						You can delete the links only if you purchased the pro version.
-						Licensing information: https://bootstrapmade.com/license/
-						Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=eBusiness
-					  -->
-					  Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-					</div>
-				  </div>
 				</div>
-			  </div>
 			</div>
 		</footer>
 
